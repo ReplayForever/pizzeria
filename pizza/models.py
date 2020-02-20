b3bf740c603608c6ee11c112b09a37f14d35ac31
ReplayForever <maslovskie77@gmail.com>
2020-02-20 19:06:29 +0300

    Add Post model and pizza view

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
+from time import time
+from django.shortcuts import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=32, db_index=True)
+    price = models.FloatField()
+    image = models.ImageField(default=None)
+    description = models.TextField(default=None)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title) + str(int(time()))
+
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('pizza', kwargs={'slug': self.slug})
--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from pizza.models import Post
+
+
+class ListPizza(View):
+    model = Post
+    included_html = 'pizza/includes/pizzas.html'
+    template = 'pizza/index.html'
+
+    def get(self, request):
+        objects = self.model.objects.all()
+
+        return render(
+            request,
+            self.included_html
+        )
